22e793a123606f19db09369c449afb67f3983875
Edgar <Edgar@AnotherFoxGuy.com>
2018-10-15 18:12:19 +0200

    :white_check_mark: Cover 100% code

--- MemoryProject/GridManager.cs
+++ MemoryProject/GridManager.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Media.Imaging;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Automation;
 
 namespace MemoryProject
 {
@@ -10,19 +11,19 @@ namespace MemoryProject
     {
         private readonly UniformGrid _grid;
 
-		public GridManager(UniformGrid grid)
+        public GridManager(UniformGrid grid)
         {
             _grid = grid;
         }
 
-		/// <summary>
-		///     Generating/Summoning the game Grid
-		/// </summary>
-		/// <param name="cols">Amount of columns</param>
-		/// <param name="rows">Amount of rows</param>
-		internal void InitializeGameGrid(int cols, int rows)
+        /// <summary>
+        ///     Generating/Summoning the game Grid
+        /// </summary>
+        /// <param name="cols">Amount of columns</param>
+        /// <param name="rows">Amount of rows</param>
+        internal void InitializeGameGrid(int cols, int rows)
         {
-			for (var row = 0; row < rows; row++)
+            for (var row = 0; row < rows; row++)
             {
                 for (var column = 0; column < cols; column++)
                 {
@@ -31,26 +32,27 @@ namespace MemoryProject
                         Source = new BitmapImage(new Uri("Images/Placeholders/100x100.png", UriKind.Relative))
                     };
 
-                    backgroundImage.MouseDown += new MouseButtonEventHandler(ClickCard);
+                    backgroundImage.Name = $"I{row}X{column}";
+                    backgroundImage.MouseDown += ClickCard;
                     Grid.SetColumn(backgroundImage, column);
                     Grid.SetRow(backgroundImage, row);
                     _grid.Children.Add(backgroundImage);
-				}
-			}
+                }
+            }
         }
-        
+
         private void ClickCard(object sender, MouseButtonEventArgs e)
         {
-            ((Image)sender).Source = new BitmapImage(new Uri("Images/Placeholders/CSharp.jpg", UriKind.Relative));
+            ((Image) sender).Source = new BitmapImage(new Uri("Images/Placeholders/CSharp.jpg", UriKind.Relative));
         }
-        
+
 
         /// <summary>
         ///     Clears the game gird 
         /// </summary>
         internal void Clear()
         {
-			_grid.Children.Clear();
-		}
+            _grid.Children.Clear();
+        }
     }
 }
--- MemoryProjectTest/UITests.cs
+++ MemoryProjectTest/UITests.cs
@@ -11,44 +11,65 @@ namespace MemoryProjectTest
     [NonParallelizable]
     public class UITests
     {
-        private Application application;
-        private string markpadLocation;
-
-        [Test]
-        public void UITest()
+        private static Application StartApplication()
         {
-            StartUp();
-            NewGame();
-            Close();
+            return Application.Launch(Path.Combine(TestContext.CurrentContext.TestDirectory, @"MemoryProject.exe"));
         }
 
-        void StartUp()
+        private static Window CreateMainGameWindow()
         {
-            var directoryName = TestContext.CurrentContext.TestDirectory;
-            markpadLocation = Path.Combine(directoryName, @"MemoryProject.exe");
-            application = Application.Launch(markpadLocation);
-            var StartUpMainWindow = application.GetWindow("Name The Game Main Menu", InitializeOption.NoCache);
-            StartUpMainWindow.Get<Button>("Settings").Click();
-        }
-
-        void NewGame()
-        {
+            var application = StartApplication();
             var mainWindow = application.GetWindow("Name The Game Main Menu", InitializeOption.NoCache);
             var NGbutton = mainWindow.Get<Button>("NewGame");
             NGbutton.Click();
             var newGameWindow = application.GetWindow("New Game", InitializeOption.NoCache);
             var NewGameGridButton = newGameWindow.Get<Button>("7X7Button");
             NewGameGridButton.Click();
-            var mainGameWindow = application.GetWindow("Name The Game", InitializeOption.NoCache);
+           return application.GetWindow("Name The Game");
+        } 
+
+
+
+        [Test, Order(1)]
+        public void NewGame()
+        {
+            var mainGameWindow = CreateMainGameWindow();
             mainGameWindow.Close();
         }
 
-        void Close()
+        [Test, Order(2)]
+        public void Settings()
         {
-            var CloseApplication = Application.Launch(markpadLocation);
-            var CloseMainWindow = CloseApplication.GetWindow("Name The Game Main Menu", InitializeOption.NoCache);
-            var CloseNGbutton = CloseMainWindow.Get<Button>("Exit");
-            CloseNGbutton.Click();
+            var application = StartApplication();
+            var startUpMainWindow = application.GetWindow("Name The Game Main Menu", InitializeOption.NoCache);
+            startUpMainWindow.Get<Button>("Settings").Click();
+            startUpMainWindow.Close();
         }
+        
+        [Test, Order(3)]
+        public void Close()
+        {
+            var application = StartApplication();
+            var mainWindow = application.GetWindow("Name The Game Main Menu", InitializeOption.NoCache);
+            var button = mainWindow.Get<Button>("Exit");
+            button.Click();
+        }
+
+        [Test]
+        public void PlayGame()
+        {
+            var mainGameWindow = CreateMainGameWindow();
+            
+            for (var row = 0; row < 7; row++)
+            {
+                for (var column = 0; column < 7; column++)
+                {
+                    mainGameWindow.Get<Image>($"I{row}X{column}").Click();
+                }
+            }
+            mainGameWindow.Close();
+        }
+        
+        
     }
 }
